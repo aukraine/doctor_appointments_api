14ea67669b1a2bb9b3a2806dc229f2922f0b6428
Andrii Ukrainets <aukra@softserveinc.com>
2023-10-27 22:23:23 +0200

    create CRUD endpoints for `Appointment` model

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,34 @@
 class AppointmentsController < ApplicationController
+  def index
+    render json: { data: Appointment.all }, status: :ok
+  end
+
+  def show
+    render json: { data: Appointment.find(appointment_params[:id]) }, status: :ok
+  end
+
+  def create
+    appointment = Appointment.create!(**appointment_params)
+
+    render json: { data: appointment }, status: :created
+  end
+
+  def update
+    appointment = Appointment.find(appointment_params[:id])
+    appointment.update!(**appointment_params)
+
+    render json: { data: appointment }, status: :ok
+  end
+
+  def destroy
+    Appointment.find(appointment_params[:id]).destroy
+
+    render :json, status: :no_content
+  end
+
+  private
+
+  def appointment_params
+    params.permit(:id, :start_time, :end_time)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :appointments
+  resources :appointments, only: [:index, :show, :create, :update, :destroy]
 end
