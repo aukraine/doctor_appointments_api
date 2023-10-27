470be199dc073f394edc0bfbb314fae3dd5a581c
Andrii Ukrainets <aukra@softserveinc.com>
2023-10-27 20:57:04 +0200

    setup `db:seed` command

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,3 @@
 class Appointment < ApplicationRecord
+  validates_presence_of :start_time, :end_time
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Appointment.destroy_all
+
+current_time = Time.current.to_i
+delta_time = 1200
+
+Appointment.create(start_time: current_time, end_time: current_time + delta_time)
+Appointment.create(start_time: current_time + delta_time, end_time: current_time + delta_time * 2)
