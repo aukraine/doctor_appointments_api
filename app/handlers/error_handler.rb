1597e02a2e956cb0ec02093a5a721e57244919e4
Andrii Ukrainets <aukra@softserveinc.com>
2023-10-29 20:22:57 +0100

    improve Error Handling

--- app/handlers/error_handler.rb
+++ app/handlers/error_handler.rb
@@ -9,14 +9,15 @@ class ErrorHandler
       when Errors::Base
         error
       when ActiveRecord::RecordNotFound
-        Errors::NotFound.new
-      when ActiveRecord::RecordNotUnique
-        Errors::UnprocessableEntity.new
+        Errors::NotFound.new(error)
+      when ActiveRecord::RecordNotUnique, ActiveRecord::RecordInvalid
+        Errors::UnprocessableEntity.new(error)
       when ArgumentError
-        Errors::BadRequest.new
+        Errors::BadRequest.new(error)
       when Pundit::NotAuthorizedError
         Errors::Forbidden.new
       else
+        # we can do not pass error message to response body if we don't want to show any internal errors in clients
         Errors::InternalServerError.new(error)
       end
     end
