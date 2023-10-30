1215422363afc41170d0188c34d1d5fcf59a7519
Andrii Ukrainets <aukra@softserveinc.com>
2023-10-30 21:02:50 +0100

    improve serializers

--- app/serializers/base_resource.rb
+++ app/serializers/base_resource.rb
@@ -2,4 +2,16 @@ class BaseResource
   include Alba::Resource
 
   root_key :data, :data
+
+  def self.formatted_time_attributes(*attrs)
+    attrs.each do |attr|
+      attribute attr do |object|
+        time = object.public_send(attr)
+
+        next unless time.is_a?(ActiveSupport::TimeWithZone)
+
+        time.utc.iso8601
+      end
+    end
+  end
 end
--- app/serializers/time_slot_resource.rb
+++ app/serializers/time_slot_resource.rb
@@ -1,5 +1,7 @@
 class TimeSlotResource < BaseResource
-  attributes :id, :start_time, :end_time
+  attributes :id, :status
+
+  formatted_time_attributes :start_time, :end_time
 
   attribute :day_of_week, &:day_of_week_before_type_cast
 end
