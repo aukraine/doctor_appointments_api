405e23f63661e8e4f6d4e152cdf6f944feddd24c
Andrii Ukrainets <aukra@softserveinc.com>
2023-10-31 20:55:34 +0100

    create new `Appointment API` swagger tests

--- spec/requests/application_spec.rb
+++ spec/requests/application_spec.rb
@@ -1,31 +1,33 @@
 require 'swagger_helper'
 
-describe 'Auth API', type: :request, swagger_doc: 'v1/swagger.json' do
+RSpec.describe 'Auth API', type: :request, swagger_doc: 'v1/swagger.json' do
+  let(:HTTP_AUTHORIZATION) { 'Bearer JWT' }
+
   path '/login' do
-    post 'Authenticate user' do
+    post 'authenticates user' do
       tags 'Auth'
+      description 'Authenticates current user'
       consumes 'application/json'
       produces 'application/json'
-      parameter name: :credentials, in: :body, schema: {
+      parameter name: :payload, in: :body, schema: {
         type: :object,
         properties: {
           email: { type: :string },
           password: { type: :string }
         },
-        required: [ 'email', 'password' ]
+        required: ['email', 'password']
       }
 
-      let(:user) { FactoryBot.create(:doctor, password: password) }
+      let(:user) { FactoryBot.create(:doctor, password: 'password') }
 
       response '200', 'authenticated' do
-        let(:password) { 'good password' }
-        let(:credentials) { { email: user.email, password: password } }
+        let(:payload) { { email: user.email, password: user.password } }
 
         run_test!
       end
 
-      response '401', 'invalid request' do
-        let(:credentials) { { email: 'foo', password: 'bar' } }
+      response '401', 'unauthorized request' do
+        let(:payload) { { email: user.email, password: 'foo' } }
 
         run_test!
       end
--- spec/requests/appointments_spec.rb
+++ spec/requests/appointments_spec.rb
@@ -1,7 +1,196 @@
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe "Appointments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Appointment API', type: :request, swagger_doc: 'v1/swagger.json' do
+  let(:HTTP_AUTHORIZATION) { 'Bearer JWT' }
+  let(:current_user) { FactoryBot.create(:patient) }
+
+  before { allow(JsonWebToken).to receive(:decode).and_return({ user_id: current_user.id }) }
+
+  path '/patient/appointments' do
+    get 'get appointments' do
+      tags 'Appointments'
+      description 'Returns list of patients appointments'
+      produces 'application/json'
+      parameter name: :started_from,
+                in: :query,
+                schema: { type: :string, format: 'date-time' },
+                required: false,
+                description: 'A filter used to filter by started date and time'
+
+      before { FactoryBot.create(:appointment, patient: current_user) }
+
+      response '200', 'appointments list' do
+        run_test!
+      end
+
+      response '401', 'unauthorized request' do
+        before { allow(JsonWebToken).to receive(:decode).and_return(nil) }
+
+        run_test!
+      end
+
+      response '403', 'forbidden action' do
+        before { allow_any_instance_of(AppointmentPolicy).to receive(:index?).and_return(false) }
+
+        run_test!
+      end
+
+      response '404', 'not found' do
+        before { allow(JsonWebToken).to receive(:decode).and_return({ user_id: nil }) }
+
+        run_test!
+      end
+
+      response '422', 'unprocessable params' do
+        let(:started_from) { 'yesterday' }
+
+        run_test!
+      end
+    end
+
+    post 'creates appointment' do
+      tags 'Appointments'
+      description 'Creates new appointment from provided data'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :payload, in: :body, schema: {
+        type: :object,
+        properties: {
+          time_slot_id: { type: :integer },
+          description: { type: :string }
+        },
+        required: ['time_slot_id']
+      }
+
+      let(:payload) { { time_slot_id: time_slot_id, description: 'description' } }
+      let(:time_slot_id) { FactoryBot.create(:time_slot).id }
+
+      response '201', 'appointment created' do
+        run_test!
+      end
+
+      response '401', 'unauthorized request' do
+        before { allow(JsonWebToken).to receive(:decode).and_return(nil) }
+
+        run_test!
+      end
+
+      response '403', 'forbidden action' do
+        before { allow_any_instance_of(AppointmentPolicy).to receive(:create?).and_return(false) }
+
+        run_test!
+      end
+
+      response '404', 'not found' do
+        let(:time_slot_id) { 0 }
+
+        run_test!
+      end
+
+      response '422', 'unprocessable params' do
+        let(:time_slot_id) { 'id' }
+
+        run_test!
+      end
+
+      response '500', 'unexpected error occurred' do
+        before { allow_any_instance_of(BookAppointment).to receive(:call).and_return({ success: false }) }
+
+        run_test!
+      end
+    end
+  end
+
+  path '/patient/appointments/{id}' do
+    parameter name: :id, in: :path, schema: { type: :integer }, required: true
+
+    put 'updates appointment' do
+      tags 'Appointments'
+      description 'Updates appointment by given ID'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :payload, in: :body, schema: {
+        type: :object,
+        properties: {
+          time_slot_id: { type: :integer },
+          description: { type: :string }
+        },
+        required: []
+      }
+
+      let(:id) { FactoryBot.create(:appointment, patient: current_user).id }
+      let(:payload) { { time_slot_id: FactoryBot.create(:time_slot).id, description: 'description' } }
+
+      response '200', 'appointment updated' do
+        run_test!
+      end
+
+      response '401', 'unauthorized request' do
+        before { allow(JsonWebToken).to receive(:decode).and_return(nil) }
+
+        run_test!
+      end
+
+      response '403', 'forbidden action' do
+        before { allow_any_instance_of(AppointmentPolicy).to receive(:update?).and_return(false) }
+
+        run_test!
+      end
+
+      response '404', 'not found' do
+        let(:id) { 0 }
+
+        run_test!
+      end
+
+      response '422', 'unprocessable params' do
+        let(:payload) { { description: nil } }
+
+        run_test!
+      end
+
+      response '500', 'unexpected error occurred' do
+        before { allow_any_instance_of(EditAppointment).to receive(:call).and_return({ success: false }) }
+
+        run_test!
+      end
+    end
+
+    delete 'deletes appointment' do
+      tags 'Appointments'
+      description 'Deletes appointment by given ID'
+      consumes 'application/json'
+      produces 'application/json'
+
+      let(:id) { FactoryBot.create(:appointment, patient: current_user).id }
+
+      response '204', 'appointment updated' do
+        run_test!
+      end
+
+      response '401', 'unauthorized request' do
+        before { allow(JsonWebToken).to receive(:decode).and_return(nil) }
+
+        run_test!
+      end
+
+      response '403', 'forbidden action' do
+        before { allow_any_instance_of(AppointmentPolicy).to receive(:destroy?).and_return(false) }
+
+        run_test!
+      end
+
+      response '404', 'not found' do
+        let(:id) { 0 }
+
+        run_test!
+      end
+
+      response '500', 'unexpected error occurred' do
+        before { allow_any_instance_of(RemoveAppointment).to receive(:call).and_return({ success: false }) }
+
+        run_test!
+      end
+    end
   end
 end
